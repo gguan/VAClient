92f051064ea025a95821a32efddaefecd6703972
wangxiangle <wang_x_le@163.com>
2018-05-30 22:17:16 +0800

    Add PersonPlayingState

---
+++ Assets/UI/humanState/PersonPlayingState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonPlayingState : PersonChatState {
+
+
+	public override void DoBeforeEntering()
+	{
+		base.DoBeforeEntering();
+		FeedBackState("state", "playing");
+	}
+}
